29e4779067377bd24794da830a9b6f08b205f600
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-06-25 16:38:15 +0100

    first step accounts exercice

---
+++ src/app/accounts/home-account/home-account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-account',
+  templateUrl: './home-account.component.html',
+  styleUrl: './home-account.component.css'
+})
+export class HomeAccountComponent {
+
+}
