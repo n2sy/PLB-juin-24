d870a16d64ab3e54eb5857b86784a447964e7c0e
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-06-25 17:38:03 +0100

    final step accounts

--- src/app/accounts/add-account/add-account.component.ts
+++ src/app/accounts/add-account/add-account.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-account',
   templateUrl: './add-account.component.html',
-  styleUrl: './add-account.component.css'
+  styleUrl: './add-account.component.css',
 })
 export class AddAccountComponent {
+  allStatus = ['', 'active', 'inactive', 'unknown'];
+  // newAccountName = '';
+  // newAccountStatus = '';
+  @Output() addEvent = new EventEmitter();
 
+  onAddAccount(inp, sel) {
+    console.log(inp.value, sel.value);
+    this.addEvent.emit({
+      name: inp.value,
+      status: sel.value,
+    });
+  }
 }
--- src/app/accounts/home-account/home-account.component.ts
+++ src/app/accounts/home-account/home-account.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-account',
   templateUrl: './home-account.component.html',
-  styleUrl: './home-account.component.css'
+  styleUrl: './home-account.component.css',
 })
 export class HomeAccountComponent {
+  allAccounts = [
+    {
+      name: 'Edouard Account',
+      status: 'active',
+    },
+    {
+      name: 'Bruno Account',
+      status: 'inactive',
+    },
+  ];
 
+  addAccount(newAccount) {
+    this.allAccounts.push(newAccount);
+  }
 }
