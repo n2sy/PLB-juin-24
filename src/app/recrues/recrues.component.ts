908c5ae82446e61d4338bee36e6b678679ad142f
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-06-26 15:39:36 +0100

    add recrues

--- src/app/recrues/recrues.component.ts
+++ src/app/recrues/recrues.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GestionRecruesService } from '../service/gestion-recrues.service';
+import { Candidat } from '../models/candidat';
 
 @Component({
   selector: 'app-recrues',
   templateUrl: './recrues.component.html',
-  styleUrl: './recrues.component.css'
+  styleUrl: './recrues.component.css',
 })
 export class RecruesComponent {
+  tab: Candidat[] = [];
+  recSer = inject(GestionRecruesService);
 
+  ngOnInit() {
+    this.tab = this.recSer.getAllRecrues();
+  }
 }
--- src/app/service/gestion-recrues.service.ts
+++ src/app/service/gestion-recrues.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Candidat } from '../models/candidat';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GestionRecruesService {
+  private listeRecrues: Candidat[] = [];
+
+  getAllRecrues() {
+    return this.listeRecrues;
+  }
+
+  addRecrue(newRecrue) {
+    if (this.listeRecrues.indexOf(newRecrue) == -1)
+      this.listeRecrues.push(newRecrue);
+    else alert('Ce candidat a déjà été recrutée');
+  }
   constructor() {}
 
   sayHi() {
